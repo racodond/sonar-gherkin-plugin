6de2ac7a2f16debba03803326420afe6facfa499
David RACODON <david.racodon@gmail.com>
2016-12-01 23:14:33 +0100

    Fix #18 New rule: Scenario should define at least one of each Given/When/Then step

--- sonar-gherkin-plugin/src/test/java/org/sonar/plugins/gherkin/GherkinProfileTest.java
+++ sonar-gherkin-plugin/src/test/java/org/sonar/plugins/gherkin/GherkinProfileTest.java
@@ -42,7 +42,7 @@ public class GherkinProfileTest {
 
     assertThat(profile.getName()).isEqualTo("SonarQube Way");
     assertThat(profile.getLanguage()).isEqualTo("gherkin");
-    assertThat(profile.getActiveRulesByRepository("gherkin")).hasSize(27);
+    assertThat(profile.getActiveRulesByRepository("gherkin")).hasSize(28);
     assertThat(validation.hasErrors()).isFalse();
   }
 
