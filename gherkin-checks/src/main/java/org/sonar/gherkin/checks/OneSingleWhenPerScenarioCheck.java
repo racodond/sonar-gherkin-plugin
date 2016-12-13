3277355abbadfe2cabde7373cd554d00d8269485
David RACODON <david.racodon@gmail.com>
2016-12-13 17:28:35 +0100

    Fix #39 There should be one single When step per scenario

---
+++ gherkin-checks/src/main/java/org/sonar/gherkin/checks/OneSingleWhenPerScenarioCheck.java
@@ -0,0 +1,65 @@
+/*
+ * SonarQube Gherkin Analyzer
+ * Copyright (C) 2016-2016 David RACODON
+ * david.racodon@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.gherkin.checks;
+
+import com.google.common.collect.ImmutableList;
+import org.sonar.check.Priority;
+import org.sonar.check.Rule;
+import org.sonar.plugins.gherkin.api.tree.BasicScenarioTree;
+import org.sonar.plugins.gherkin.api.tree.StepTree;
+import org.sonar.plugins.gherkin.api.tree.Tree;
+import org.sonar.plugins.gherkin.api.visitors.SubscriptionVisitorCheck;
+import org.sonar.plugins.gherkin.api.visitors.issue.PreciseIssue;
+import org.sonar.squidbridge.annotations.ActivatedByDefault;
+import org.sonar.squidbridge.annotations.SqaleConstantRemediation;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Rule(
+  key = "one-single-when-per-scenario",
+  name = "There should be one single When step per scenario",
+  priority = Priority.MAJOR,
+  tags = {Tags.DESIGN})
+@SqaleConstantRemediation("15min")
+@ActivatedByDefault
+public class OneSingleWhenPerScenarioCheck extends SubscriptionVisitorCheck {
+
+  @Override
+  public List<Tree.Kind> nodesToVisit() {
+    return ImmutableList.of(Tree.Kind.SCENARIO, Tree.Kind.SCENARIO_OUTLINE);
+  }
+
+  @Override
+  public void visitNode(Tree tree) {
+    List<StepTree> whenSteps = ((BasicScenarioTree) tree).steps()
+      .stream()
+      .filter(s -> s.type() == StepTree.StepType.WHEN)
+      .collect(Collectors.toList());
+
+    if (whenSteps.size() > 1) {
+      PreciseIssue issue = addPreciseIssue(whenSteps.get(0), "Merge all the When steps or split the scenario up in multiple scenarios.");
+      for (int i = 1; i < whenSteps.size(); i++) {
+        issue.secondary(whenSteps.get(i), "When step");
+      }
+    }
+  }
+
+}
--- sonar-gherkin-plugin/src/test/java/org/sonar/plugins/gherkin/GherkinRulesDefinitionTest.java
+++ sonar-gherkin-plugin/src/test/java/org/sonar/plugins/gherkin/GherkinRulesDefinitionTest.java
@@ -37,7 +37,7 @@ public class GherkinRulesDefinitionTest {
 
     assertThat(repository.name()).isEqualTo("SonarQube");
     assertThat(repository.language()).isEqualTo("gherkin");
-    assertThat(repository.rules()).hasSize(44);
+    assertThat(repository.rules()).hasSize(45);
 
     RulesDefinition.Rule lineLengthRule = repository.rule(TabCharacterCheck.class.getAnnotation(Rule.class).key());
     assertThat(lineLengthRule).isNotNull();
