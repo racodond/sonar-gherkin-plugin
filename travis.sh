3b81a06ab3c59df2c46a5d2cdf8f69b4f87e602d
David RACODON <david.racodon@gmail.com>
2018-02-15 09:32:25 +0100

    Activate SonarQube branch analysis

--- travis.sh
+++ travis.sh
@@ -4,19 +4,20 @@ set -euo pipefail
 
 mvn -B clean install -Pits -Dsonar.runtimeVersion=$SQ_VERSION
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$SQ_VERSION" == "LTS" ]; then
-  mvn -B sonar:sonar \
-      -Dsonar.host.url=https://sonarcloud.io \
-      -Dsonar.analysis.mode=preview \
-      -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
-      -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
-      -Dsonar.github.oauth=$GITHUB_TOKEN \
-      -Dsonar.login=$SONAR_TOKEN \
-      -Dsonar.organization=racodond-github
+if [ "$SQ_VERSION" == "LTS" ]; then
 
-elif [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$SQ_VERSION" == "LTS" ]; then
-  mvn -B sonar:sonar \
-      -Dsonar.host.url=https://sonarcloud.io \
-      -Dsonar.login=$SONAR_TOKEN \
-      -Dsonar.organization=racodond-github
+    mvnCommand='mvn -B sonar:sonar'
+    commonArgs="-Dsonar.host.url=https://sonarcloud.io -Dsonar.login=$SONAR_TOKEN -Dsonar.organization=racodond-github"
+    githubArgs="-Dsonar.analysis.mode=preview -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST -Dsonar.github.repository=$TRAVIS_REPO_SLUG -Dsonar.github.oauth=$GITHUB_TOKEN"
+    branchArgs=''
+
+    if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+        if [ "$TRAVIS_BRANCH" != "master" ]; then
+           branchArgs="-Dsonar.branch.name=$TRAVIS_BRANCH -Dsonar.branch.target=master"
+        fi
+        eval $mvnCommand $commonArgs $branchArgs
+    else
+        eval $mvnCommand $commonArgs $githubArgs
+    fi
+
 fi
